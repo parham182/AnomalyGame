888efcfb1502cb570e530f46386b69e74b31bdbe
DevMood <parhamqop@gmail.com>
2026-07-02 01:50:46 +0330

    PlayerMovement

---
+++ AnomalyGame/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
+{
+    public InputActionReference mouse;
+    [SerializeField] float sensitivity = 120f;
+    [SerializeField] Transform playerBody;
+
+    [SerializeField] float xClampMin = -90f;
+    [SerializeField] float xClampMax = 90f;
+
+    float xRotation;
+    float yRotation;
+
+
+    void OnEnable()
+    {
+        mouse.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        mouse.action.Disable();
+    }
+
+    void Start()
+    {
+        if (playerBody == null)
+        {
+            playerBody = transform.parent;
+        }
+
+        // Initialize from current scene rotation to avoid snapping on first frame.
+        xRotation = transform.localEulerAngles.x;
+        if (xRotation > 180f)
+        {
+            xRotation -= 360f;
+        }
+
+        if (playerBody != null)
+        {
+            yRotation = playerBody.localEulerAngles.y;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void LateUpdate()
+    {
+        LookAround();
+    }
+
+    void LookAround()
+    {
+        if (Cursor.lockState != CursorLockMode.Locked)
+        {
+            return;
+        }
+
+        Vector2 look = mouse.action.ReadValue<Vector2>();
+
+        float mouseX = look.x * sensitivity * Time.deltaTime;
+        float mouseY = look.y * sensitivity * Time.deltaTime;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        
+        xRotation = Mathf.Clamp(xRotation, xClampMin, xClampMax);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        if (playerBody != null)
+        {
+            playerBody.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+        }
+    }
+
+}
