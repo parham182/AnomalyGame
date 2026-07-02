97f41c949a48ead6e05847853114b519ddd73bc2
DevMood <parhamqop@gmail.com>
2026-07-02 18:35:17 +0330

    NPC

---
+++ AnomalyGame/Assets/Scripts/NPC/NpcMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcMovement : MonoBehaviour
+{
+    [Header("Targets")]
+    [SerializeField] Transform normalTarget;
+    [SerializeField] Transform player; //For Anomaly
+
+    NavMeshAgent navMeshAgent;
+
+    bool isStop = false;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+        if (normalTarget != null )
+        {
+            navMeshAgent.isStopped = isStop;
+            navMeshAgent.SetDestination(normalTarget.position);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isStop = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isStop = false;
+        }
+    }
+}
