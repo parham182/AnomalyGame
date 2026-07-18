f4d822e2711df93cdc15f4a6814b60086dde8b14
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2026-07-18 18:47:02 +0330

    texture color

--- AnomalyGame/Assets/Scripts/Player/PlayerMovement.cs
+++ AnomalyGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,10 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] Transform cam;
-    [SerializeField] AudioSource walksound;
+    [SerializeField] AudioSource walkAudioSource;
     [SerializeField] AudioClip[] walkClips;
-    [SerializeField] Vector2 walkVolumeRange = new Vector2(0.4f, 1f);
-    [SerializeField] float walkVolumeChangeInterval = 0.25f;
     [SerializeField] float gravity = -20f;
     private CharacterController controller;
 
@@ -17,15 +15,13 @@ public class PlayerMovement : MonoBehaviour
 
     public bool canMove = true;
     public bool IsMoving { get; private set; }
-    bool wasMoving;
-    float nextWalkVolumeTime;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        if (walksound != null)
+        if (walkAudioSource != null)
         {
-            walksound.loop = true;
+            walkAudioSource.loop = true;
         }
     }
 
@@ -37,35 +33,6 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         playerMove();
-        IsMoving = moveInput.sqrMagnitude > 0.001f;
-        if (walksound != null)
-        {
-            if (IsMoving && !wasMoving)
-            {
-                AudioClip clipToPlay = null;
-                if (walkClips != null && walkClips.Length > 0)
-                {
-                    int index = Random.Range(0, walkClips.Length);
-                    clipToPlay = walkClips[index];
-                }
-
-                if (clipToPlay != null)
-                {
-                    walksound.clip = clipToPlay;
-                    SetRandomWalkVolume();
-                    nextWalkVolumeTime = Time.time + walkVolumeChangeInterval;
-                    walksound.Play();
-                }
-            }
-            else if (!IsMoving && wasMoving)
-                walksound.Stop();
-        }
-        if (IsMoving && walksound != null && walksound.isPlaying && Time.time >= nextWalkVolumeTime)
-        {
-            SetRandomWalkVolume();
-            nextWalkVolumeTime = Time.time + walkVolumeChangeInterval;
-        }
-        wasMoving = IsMoving;
     }
 
     private void playerMove()
@@ -83,19 +50,4 @@ public class PlayerMovement : MonoBehaviour
         Vector3 finalMove = velocity * Time.deltaTime;
         controller.Move(finalMove);
     }
-
-    void SetRandomWalkVolume()
-    {
-        float min = Mathf.Clamp01(walkVolumeRange.x);
-        float max = Mathf.Clamp01(walkVolumeRange.y);
-        if (max < min)
-        {
-            float temp = min;
-            min = max;
-            max = temp;
-        }
-        walksound.volume = Random.Range(min, max);
-    }
-
-
 }
