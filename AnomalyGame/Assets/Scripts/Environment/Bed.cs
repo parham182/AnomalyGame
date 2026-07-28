d22e1e2e6433e897fa222ebdddcf2b7e94a4d359
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2026-07-28 14:53:38 +0330

    حل باگ نور نداشتن سقف

--- AnomalyGame/Assets/Scripts/Environment/Bed.cs
+++ AnomalyGame/Assets/Scripts/Environment/Bed.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Bed : MonoBehaviour, IInteractable
 {
     [SerializeField] AudioClip sleepSound;
+    [SerializeField] PillChoseManager pillChoseManager;
     public void Interact()
     {
         if (Player.instance.hasPills)
         {
-            SoundManager.instance.PlaySoundEffect(sleepSound);
-            Player.instance.Sleep();
+            // SoundManager.instance.PlaySoundEffect(sleepSound);
+            // open pill selecting options
+            pillChoseManager.OpenOptions();
         } else
         {
             NotifManager.instance.ShowNotif("بدون قرص نمیتونم بخوابم", 3);
