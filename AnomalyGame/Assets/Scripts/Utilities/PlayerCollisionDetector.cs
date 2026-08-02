9b7ed24051944f8dd1c08aa48de7fe280f5faa20
DevMood <parhamqop@gmail.com>
2026-08-02 21:22:34 +0330

    Fix Bugs

--- AnomalyGame/Assets/Scripts/Utilities/PlayerCollisionDetector.cs
+++ AnomalyGame/Assets/Scripts/Utilities/PlayerCollisionDetector.cs
@@ -5,13 +5,21 @@ public class PlayerCollisionDetector : MonoBehaviour
 {
     public UnityEvent onAction;
 
+    int counter = 0;
     public void Execute()
     {
+        if (counter > 1) return;
+       
         onAction?.Invoke();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player") Execute();
+        if (other.tag == "Player")
+        {
+            counter++;
+            Execute();
+        }
     }
+
 }
