82c96dd4aba28f9805ea6d0c20409566a9dc8379
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2026-08-06 18:42:45 +0330

    louder jumpscare sound

--- AnomalyGame/Assets/Scripts/Slender.cs
+++ AnomalyGame/Assets/Scripts/Slender.cs
@@ -5,7 +5,6 @@ public class Slender : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] AudioSource audioSource;
-    [SerializeField] AudioClip screamClip;
     [SerializeField] Transform jumpscareCameraPos;
 
     private void OnTriggerEnter(Collider collider)
