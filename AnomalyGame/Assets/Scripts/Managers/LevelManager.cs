fbc03addaf877568990e4cea88882c8db47e1d80
DevMood <parhamqop@gmail.com>
2026-08-10 01:59:16 +0330

    Update LevelManager.cs

--- AnomalyGame/Assets/Scripts/Managers/LevelManager.cs
+++ AnomalyGame/Assets/Scripts/Managers/LevelManager.cs
@@ -88,7 +88,7 @@ public class LevelManager : MonoBehaviour
             // remove anomaly from ShowableLevels
             ShowableLevels.Remove(selectedHouse);
             ShowedLevels.Add(selectedHouse);
-            anomalyChance -= 0.15f;
+            anomalyChance -= 0.1f;
         }
         else
         {
